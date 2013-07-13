845b2201e0fbc37ddc5d750b54e3c89b8843d03c
pduszak <duszak.paul@gmail.com>
2013-07-13 12:59:30 -0500

    Never ever use Tem's phone. Maps working.

--- src/com/depaul/divvyup/MainActivity.java
+++ src/com/depaul/divvyup/MainActivity.java
@@ -2,12 +2,15 @@ package com.depaul.divvyup;
 
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.view.View.OnClickListener;
 import android.view.WindowManager;
 import android.app.Activity;
+import android.content.Intent;
 
 public class MainActivity extends Activity {
-
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +18,18 @@ public class MainActivity extends Activity {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        
+        findViewById(R.id.button1).setOnClickListener(
+				new OnClickListener() {
+
+					@Override
+					public void onClick(View v) {
+						// TODO Auto-generated method stub
+						Intent i = new Intent(getApplicationContext(),DAMAP.class);
+                        startActivity(i);
+					}
+
+				});
     }
     
 }
